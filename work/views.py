165e0e2c90052cf5c1ab6cb99721b458e2872a9e
wangsong <wangsong1299@163.com>
2015-10-18 12:16:10 +0800

    基本面2

--- work/views.py
+++ work/views.py
@@ -4,14 +4,16 @@
 #encoding=utf-8
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from blog.models import Text
+from work.models import Text
 import json
  
 def index(request):
-	textArr = Text.objects.all().order_by('-create_time')[:3]
+	textArr = Text.objects.all().order_by('-create_time')[:5]
 	text1={}
 	text2={}
 	text3={}
+	text4={}
+	text5={}
 	text1[0]=textArr[0].id
 	text1[1]=textArr[0].title
 	text1[2]=textArr[0].abstract
@@ -26,7 +28,15 @@ def index(request):
 	text3[1]=textArr[2].title
 	text3[2]=textArr[2].abstract
 	text3[3]=textArr[2].tag
-	return render_to_response('work.html',{'text1':json.dumps(text1),'text2':json.dumps(text2),'text3':json.dumps(text3)})
+	text4[0]=textArr[3].id
+	text4[1]=textArr[3].title
+	text4[2]=textArr[3].abstract
+	text4[3]=textArr[3].tag
+	text5[0]=textArr[4].id
+	text5[1]=textArr[4].title
+	text5[2]=textArr[4].abstract
+	text5[3]=textArr[4].tag
+	return render_to_response('work.html',{'text1':json.dumps(text1),'text2':json.dumps(text2),'text3':json.dumps(text3),'text4':json.dumps(text4),'text5':json.dumps(text5)})
 
 def article(request,id):
 	text = Text.objects.filter(id=id)[0]
